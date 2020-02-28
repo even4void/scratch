93e81d62b5108d19ece26e94a46d412362c655a5
chl <chl@aliquote.org>
2020-02-28 15:31:40 +0100

    ↻ wip

--- lisp/cryptopals.rkt
+++ lisp/cryptopals.rkt
@@ -1,5 +1,8 @@
 #lang racket
 
+;; Cryptopals crypto challenges
+;; https://cryptopals.com
+
 ;; -----
 ;; Set 1
 ;; -----
@@ -15,5 +18,17 @@
 ;; Problem 2
 (define a "1c0111001f010100061a024b53535009181c")
 (define b "686974207468652062756c6c277320657965")
+
+(define (split-pairs lst)
+  (cond
+    [(null? lst) lst]
+    [(<= (length lst) 2) (cons lst '())]
+    [else (cons (take lst 2) (split-pairs (cddr lst)))]))
+
+(define aa (split-pairs (string->list a)))
+(define bb (split-pairs (string->list b)))
+
+; or
+(bytes->list (hex-string->bytes a))
 
 (define zip (lambda (x y) (map list x y)))
