a8e28ef6fe96a83f5e2141be9757e2193fec6be3
chl <chl@aliquote.org>
2021-11-23 09:13:00 +0100

    wip

---
+++ rng.lisp
@@ -0,0 +1,14 @@
+(defun box-muller ()
+  (loop
+    for x1 = (1- (* 2.0d0 (random 1d0)))
+    for x2 = (1- (* 2.0d0 (random 1d0)))
+    for w = (+ (* x1 x1) (* x2 x2))
+    while (>= w 1d0)
+    finally
+    (let ((w (sqrt (/ (* -2d0 (log w)) w))))
+          (return (values (* x1 w) (* x2 w))))))
+
+(defun rnorm (mean std)
+  (+ (* (box-muller) std) mean))
+
+(rnorm 10 2)
