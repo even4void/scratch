0683e5e0f50708c41696acbf27af654fab9f74c8
chl <chl@aliquote.org>
2022-07-29 09:10:22 +0200

    Fix typo

--- t-test.scm
+++ t-test.scm
@@ -1,29 +1,20 @@
-(import (chicken foreign))
+(import statistics)
 
-(foreign-declare "#include <gsl/gsl_sf_gamma.h>")
+(t-test-two-sample 12 2 24 11 1.5 24 #f)
 
-(define beta-incomplete
-  (foreign-lambda double "gsl_sf_beta_inc" double double double))
+(define (pooled-sd s1 n1 s2 n2)
+    (sqrt (/ (+ (* (- n1 1) (* s1 s1))
+                (* (- n2 1) (* s2 s2)))
+             (+ n1 n2 -2))))
+(define sp (pooled-sd 2 24 1.5 24))
 
-(define (p-value stat df two-sided)
-  (set! stat (exact->inexact stat))
-  (set! df (exact->inexact df))
-  (let ((a (beta-incomplete (* 0.5 df) 0.5 (/ df (+ df (* stat stat))))))
-    (if (two-sided a)
-      (if (positive? stat)
-        (* 0.5 a)
-        (- 1.0 (* 0.5 a))))))
+(define se (* sp (sqrt (/ 1 12))))
+(define qt (t-distribution 46 0.975))
+(values (- 1 (* qt se)) (+ 1 (* qt se)))
 
-(define (t-test x1 s1 n1 x2 s2 n2 two-sided)
-  (let ((stat #f)
-        (df #f))
-    (let ((s (sqrt (/ (+ (* (- n1 1) (* s1 s1)
-                            (* (- n2 1) (* s2 s2))))
-                      (+ n1 n2 -2)))))
-      (set! stat (/ (- x1 x2
-                       (* s (sqrt (+ (/ n1) (/ n2)))))))
-      (set! df (+ n1 n2 -2)))
-    (p-value stat df two-sided)))
-
-;; test
-(t-test 12 2 24 11 1.5 24 #t)
+(define se (sqrt (+ (/ (* 2 2) 24) (/ (* 1.5 1.5) 24))))
+(define nu (/ (square (+ (/ (* 2 2) 24) (/ (* 1.5 1.5) 24)
+                              (+ (/ (square (/ (* 2 2) 24)) (- 24 1))
+                                 (/ (square (/ (* 1.5 1.5) 24)) (- 24 1)))))))
+(define qt (t-distribution nu 0.975))
+(values (- 1 (* qt se)) (+ 1 (* qt se)))
