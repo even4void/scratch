3b02a2d9c47afe609a7963eb89da8e9b9b9bbe98
chl <chl@aliquote.org>
2023-03-02 10:39:08 +0100

    Add a handy macro from Rainer Joswig

--- macros.lisp
+++ macros.lisp
@@ -15,3 +15,12 @@
     (fact-iter 1 1 n))
 
 (factorial 1000)
+
+;; https://stackoverflow.com/a/72355135
+(defmacro arange ((var n start
+                       &optional (result nil)
+                       &body body))
+  `(loop for ,var from ,start
+         repeat ,n
+         do (progn ,@body)
+         finally (return ,result)))
